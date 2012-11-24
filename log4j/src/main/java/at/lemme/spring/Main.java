33c5adc2fda96f0c7f3d77cff9237fbed717cc55
tlemme <thomas.lemme@rise-world.com>
2012-11-24 16:59:01 +0100

    add log4j project

--- log4j/src/main/java/at/lemme/spring/Main.java
+++ log4j/src/main/java/at/lemme/spring/Main.java
@@ -2,8 +2,6 @@ package at.lemme.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import at.lemme.spring.model.Pojo;
-
 public class Main {
 
   public static void main(String[] args) {
@@ -11,12 +9,8 @@ public class Main {
 
     TestBean bean = ctx.getBean(TestBean.class);
 
-    Pojo pojo1 = bean.insertObject();
-    System.out.println(pojo1);
-    Long id = pojo1.getId();
-    
-    Pojo pojo2 = bean.findObject(id);
-    System.out.println(pojo2);
+    bean.logSomething();
+
   }
 
 }
--- log4j/src/main/java/at/lemme/spring/TestBean.java
+++ log4j/src/main/java/at/lemme/spring/TestBean.java
@@ -1,29 +1,25 @@
 package at.lemme.spring;
 
-import javax.inject.Inject;
+import javax.annotation.PostConstruct;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import at.lemme.spring.dao.Dao;
-import at.lemme.spring.model.Pojo;
 
 @Component("testBean")
 public class TestBean {
-
-  @Inject
-  private Dao dao;
-
-  @Transactional
-  public Pojo insertObject() {
-    Pojo pojo = new Pojo();
-    pojo.setName("testObject");
-    dao.persist(pojo);
-    return pojo;
-  }
   
-  public Pojo findObject(Long id){
-    return dao.find(Pojo.class, id);
+  private static final Logger log = Logger.getLogger(TestBean.class);
+  
+  @PostConstruct
+  public void init(){
+    log.debug("TestBean created");
+  }
+
+  public void logSomething(){
+    log.debug("log something");
+    log.info("log something");
+    log.warn("log something");
+    log.error("log something");
   }
 
 }
