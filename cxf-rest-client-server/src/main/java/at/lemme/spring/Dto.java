a5967528667c7c1e4bc76a92bbae143d440984e1
tlemme <thomas.lemme@rise-world.com>
2012-11-28 01:00:24 +0100

    cxf-rest-client-server cleanup

--- cxf-rest-client-server/src/main/java/at/lemme/spring/Dto.java
+++ cxf-rest-client-server/src/main/java/at/lemme/spring/Dto.java
@@ -10,7 +10,6 @@ import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-//@XmlType(name = "")
 @XmlRootElement()
 public class Dto {
 
